410fdff8ab5344f3089842ed69345c89fdfee177
ashsic <99445200+ashsic@users.noreply.github.com>
2023-10-27 10:14:35 -0400

    TOP JS exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+    let args = Array.from(arguments);
+    let i = 1;
+    while (i < args.length){
+        if (args[0].includes(args[i])){
+            args[0].splice(args[0].indexOf(args[i]), 1);
+        }
+        i++;
+        }
+    return args[0];
 };
 
 // Do not edit below this line
